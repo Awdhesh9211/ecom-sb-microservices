32b768b190e097cbacaa7ae047db50fa61cc635f
Awdhesh9211 <gaundawdhesh9211@gmail.com>
2026-01-29 13:54:51 +0530

    done config server file based configuration

---
+++ orderms/src/main/java/com/ecommerce/orderms/controller/MessageController.java
@@ -0,0 +1,22 @@
+package com.ecommerce.orderms.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//for testing config server
+@RestController
+public class MessageController {
+
+
+    @Value("${app.message}")
+    private  String message;
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return message;
+    }
+
+}
